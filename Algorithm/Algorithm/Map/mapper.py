7e4b8277b0a4d15878752bf9ce8fd246f39ed4fb
vrohith24 <70521157+vrohith24@users.noreply.github.com>
2021-01-06 13:10:50 +0530

    Update mapper.py

--- Algorithm/Algorithm/Map/mapper.py
+++ Algorithm/Algorithm/Map/mapper.py
@@ -1,12 +1,11 @@
-def mapper(cent, datapoint):
-    min = Find_dist(datapoint,cent[0])
+def mapper(cent,center,datapoint, datapoint1):
+    min1 = Find_dist(datapoint1,cent[0])
     closest = cent[0]
     for i in range(1,len(cent)):
-        curr = Find_dist(datapoint,cent[i])
-        if curr < min:
-            min = curr
+        curr = Find_dist(datapoint1,cent[i])
+        if curr < min1:
+            min1 = curr
             closest = cent[i]
-#return key = centre, datapoint
-
-    return closest,datapoint
-
+    center.append(closest)
+    datapoint.append(datapoint1)
+    return center,datapoint
